8518d91deca47aab4eaac73e87bdbece1e68bc85
YasserDalali <yasser.dalali.personal@gmail.com>
2024-11-11 23:10:03 +0100

    navbar edit test

--- src/components/UI/NavBar.jsx
+++ src/components/UI/NavBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NavBar() {
   return (
     <nav className='flex justify-between items-center p-ws md:p-ws_lg'>
-        <h3 className='font-major font-bold text-xl text-primary md:text-3xl'>Touch ATM <span className='text-secondary'>*</span></h3>
+        <h3 className='font-major font-bold text-lg text-primary md:text-3xl'>Touch ATM <span className='text-secondary'>*</span></h3>
         <button className='bg-primary text-white px-8 py-2 rounded-3xl font-major font-bold'>Login</button>
     </nav>
   )
